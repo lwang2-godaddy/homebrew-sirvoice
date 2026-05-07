d75f08d40f8fef9e008c4fd2de4a35282db3b008
lwang2-godaddy <112432215+lwang2-godaddy@users.noreply.github.com>
2026-05-07 11:43:25 -0700

    Update to v1.0.5

--- Casks/sirvoice.rb
+++ Casks/sirvoice.rb
@@ -1,6 +1,6 @@
 cask "sirvoice" do
-  version "1.0.4"
-  sha256 "218ea6cd69d51b528a5ed7a328a9477e2cd623255c7c6c8f2501bca569bf7d62"
+  version "1.0.5"
+  sha256 "bc83fd1e0d9d5c153190f1e63b68426dd56a2718607421d7e87d4515415dbfa7"
 
   url "https://github.com/lwang2-godaddy/sirvoice/releases/download/v#{version}/SirVoice-#{version}.dmg"
   name "SirVoice"
